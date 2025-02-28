bfcc5a6765a23061e91482caad2886c733883082
bonsaivaras <aivaras.bonckus@gmail.com>
2025-02-28 12:00:02 +0200

    Fix asset loading in SplashScreen for production build

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import Button from './Button';
+// Import assets
+import logoSrc from '../assets/Logo-scrapchase.svg';
+import carSrc from '../assets/car.png';
+import engineSrc from '../assets/Property 1=Engine.png';
+import steeringWheelSrc from '../assets/Property 1=Steering wheel.png';
+import turboSrc from '../assets/Property 1=Turbo.png';
+import wheelSrc from '../assets/Property 1=Wheel.png';
+import springSrc from '../assets/Property 1=Spring coil.png';
+import batterySrc from '../assets/Property 1=Battery.png';
+import headlightSrc from '../assets/Property 1=Headlight.png';
+import mirrorSrc from '../assets/Property 1=Mirror.png';
+import variantSrc from '../assets/Property 1=Variant9.png';
 
 interface SplashScreenProps {
   onStartGame: () => void;
@@ -8,7 +20,7 @@ interface SplashScreenProps {
 const SplashScreen: React.FC<SplashScreenProps> = ({ onStartGame }) => {
   return (
     <div className="splash-screen">
-      <img className="splash-logo" src="src/assets/Logo-scrapchase.svg" alt="Scrap Chase Logo" />
+      <img className="splash-logo" src={logoSrc} alt="Scrap Chase Logo" />
       <div className="splash-content">
         <h1 className="splash-title">Scrap Chase</h1>
         <p className="splash-description">
@@ -23,16 +35,16 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ onStartGame }) => {
         </Button>
       </div>
       <div className="splash-screen-image">
-        <img className="splash-car" src="src/assets/car.png" alt="Car" />
-        <div className="part-image"><img src="src/assets/Property 1=Engine.png" alt="Engine" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Steering wheel.png" alt="Steering Wheel" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Turbo.png" alt="Turbo" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Wheel.png" alt="Wheel" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Spring coil.png" alt="Spring" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Battery.png" alt="Battery" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Headlight.png" alt="Headlight" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Mirror.png" alt="Mirror" /></div>
-        <div className="part-image"><img src="src/assets/Property 1=Variant9.png" alt="Variant" /></div>
+        <img className="splash-car" src={carSrc} alt="Car" />
+        <div className="part-image"><img src={engineSrc} alt="Engine" /></div>
+        <div className="part-image"><img src={steeringWheelSrc} alt="Steering Wheel" /></div>
+        <div className="part-image"><img src={turboSrc} alt="Turbo" /></div>
+        <div className="part-image"><img src={wheelSrc} alt="Wheel" /></div>
+        <div className="part-image"><img src={springSrc} alt="Spring" /></div>
+        <div className="part-image"><img src={batterySrc} alt="Battery" /></div>
+        <div className="part-image"><img src={headlightSrc} alt="Headlight" /></div>
+        <div className="part-image"><img src={mirrorSrc} alt="Mirror" /></div>
+        <div className="part-image"><img src={variantSrc} alt="Variant" /></div>
       </div>
     </div>
   );
